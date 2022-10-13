3d6bb13938a7958a78006e500942ca1f798a10c9
Pedro Neves <pedronevesbezerra@gmail.com>
2022-10-13 14:20:46 -0300

    Project created

---
+++ backend/src/main/java/com/devhu/devhu/DevhuApplication.java
@@ -0,0 +1,13 @@
+package com.devhu.devhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevhuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devhu/devhu/DevhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhu.devhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
