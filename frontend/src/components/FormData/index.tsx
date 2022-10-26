04733cec342d2b65b487109d5283e3ca526a1945
Pedro Neves <pedronevesbezerra@gmail.com>
2022-10-26 18:20:02 -0300

    implement useForm

--- frontend/src/components/FormData/index.tsx
+++ frontend/src/components/FormData/index.tsx
@@ -1,53 +1,46 @@
 import './style.css'
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm, Resolver } from "react-hook-form";
+import axios from 'axios';
 
-type FormValues = {
-   name: String,
-   birthDate: String,
-   date: String
-  };
+// type FormValues = {
+//    name: String;
+//    birthDate: String;
+//   };
 
-const resolver: Resolver<FormValues> = async (values) => {
-    return {
-      values: values,
-      errors: !values.name
-      ? {
-          name: {
-            type: "required",
-            message: "This is required."
-          }
-        }
-      : {}
-    };
-  };
+// const resolver: Resolver<FormValues> = async (values) => {
+//     return {
+//       values: values,
+//       errors: !values.name
+//       ? {
+//           name: {
+//             type: "required",
+//             message: "This is required."
+//           }
+//         }
+//       : {}
+//     };
+//   };
 
 function FormData() {
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-      } = useForm<FormValues>({
-        resolver: resolver
-      });
+    const {register, handleSubmit} = useForm();
 
-    const [state, setState] = useState<FormValues>();
-    const onSubmit = handleSubmit((data) => setState(state => [data]));
+    const onSubmit = (e) => {
+      console.log(e);
+    }
 
     return(
-        <form onSubmit = {onSubmit}>
+        <form onSubmit = {handleSubmit(onSubmit)}>
             <div className="form">
                 <div className="form-group-name">
                     <p>Nome completo</p>
                     <input className="form-control" type="text" placeholder="Nome completo" {...register("name")}/>
-                    {errors?.name && <p>{errors.name.message}</p>}
                 </div>
                 <div className="form-group-birth">
                     <p>Data de nascimento</p>
                     <input className="form-control-date" type="text" placeholder="dia/mês/ano" {...register("birthDate")}/>
-                    {errors?.birthDate && <p>{errors.birthDate.message}</p>}
                 </div>
                 <button type="submit">Salvar</button> 
             </div>
